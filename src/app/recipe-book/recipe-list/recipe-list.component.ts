f61fc8b94dc206a4a7c0914fca54e7a3bfbaa1e4
SmRafialam <smrafialam.com>
2023-01-07 16:47:48 +0600

    Shopping-item & Outputting list

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -8,7 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] = [{name:'A Test Recipe', description:'This is simply a test',imagePath:'https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/british_shakshuka_26737_16x9.jpg' }] 
+  recipes: Recipe[] = [{name:'A Test Recipe', description:'This is simply a test',imagePath:'https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/british_shakshuka_26737_16x9.jpg' },{name:'A Test Recipe2', description:'This is simply a test2',imagePath:'https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/british_shakshuka_26737_16x9.jpg' }] 
   
   // recipes: Recipe[] = [
   //   new Recipes('A Test Recipe','This is simply a test','https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/british_shakshuka_26737_16x9.jpg')
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from './ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
+  ingredients:Ingredient[] = [{name:'apples',amount:10},{name:'tomatos',amount:5}];
   constructor() { }
 
   ngOnInit(): void {
