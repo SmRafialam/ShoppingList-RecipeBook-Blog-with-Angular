2375e366ce68401cfc8f62f03e3c9f1501d5b8ba
SmRafialam <smrafialam@gmail.com>
2023-01-14 16:15:00 +0600

    dropdown with 'Directives'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail
 import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
 import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     RecipeDetailComponent,
     RecipeItemComponent,
     RecipeListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    DropdownDirective
     
   ],
   imports: [
